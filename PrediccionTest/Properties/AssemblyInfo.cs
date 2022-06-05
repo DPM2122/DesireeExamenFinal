5a8826effa7ab3084aa18b761ac4dd3c6c52e1ed
Desiree Moya <desmp7@gmail.com>
2022-06-05 18:34:21 +0200

    Proyecto preparado para que el usuario 2 lo acabe

---
+++ PrediccionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrediccionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrediccionTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1ee8f48-4c6f-4fef-a74c-28fe76d548c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PrediccionTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinalNS;
+
+
+namespace PrediccionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PruebaTemperaturaFueraRango() //Esta prueba pretende sacar un error si la temperatura máxima o mínima es mayor o menor de lo introducido en la clase
+        {
+            Prediccion miPrediccionPrueba = new Prediccion();
+
+            List<double> dia1 = new List<double>(),
+                         dia2 = new List<double>(),
+                         dia3 = new List<double>();
+
+            dia1.Add(12.5); dia1.Add(16.5); dia1.Add(21); dia1.Add(17); dia1.Add(15);
+            dia2.Add(13); dia2.Add(15); dia2.Add(19.5); dia2.Add(16.5); dia2.Add(14);
+            dia3.Add(14.5); dia3.Add(18.5); dia3.Add(23); dia3.Add(20); dia3.Add(17.5);
+
+            miPrediccionPrueba.TempMax = 1001;
+
+
+            if (miPrediccionPrueba.TempMax <= 1000)
+            {
+                miPrediccionPrueba.PrediccionTemperatura(dia1, dia2, dia3);
+
+
+                Console.WriteLine("Temperatura Celsius: " + miPrediccionPrueba.TempCelsius);
+                Console.WriteLine("Temperatura Farenheit: " + miPrediccionPrueba.TempFarenheit);
+                Console.WriteLine("Máxima: " + miPrediccionPrueba.TempMax);
+                Console.WriteLine("Mínima: " + miPrediccionPrueba.TempMin);
+
+                Console.ReadKey();
+
+
+            }
+            else
+            {
+
+                throw new ArgumentOutOfRangeException("La temperatura máxima es incorrecta");
+
+            }
+
+        }    
+        
+    }
+}
