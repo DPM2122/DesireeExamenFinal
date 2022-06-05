e5f41a8935ae890743daa69f402b89c5cf5b46a5
Desi MP <desmp7bis@gmail.com>
2022-06-05 20:53:58 +0200

    ya

--- PrediccionTest/UnitTest1.cs
+++ PrediccionTest/UnitTest1.cs
@@ -19,21 +19,6 @@ namespace PrediccionTest
                          dia3 = new List<double>();
 
 
-            dia1.Add(12.5);
-            dia1.Add(16.5);
-            dia1.Add(21);
-            dia1.Add(17);
-            dia1.Add(15);
-            dia2.Add(13);
-            dia2.Add(15);
-            dia2.Add(19.5);
-            dia2.Add(16.5);
-            dia2.Add(14);
-            dia3.Add(14.5);
-            dia3.Add(18.5);
-            dia3.Add(23);
-            dia3.Add(20);
-            dia3.Add(17.5);
 
             dia1.Add(12.5); dia1.Add(16.5); dia1.Add(21); dia1.Add(17); dia1.Add(15); //media1 16.4
             dia2.Add(13); dia2.Add(15); dia2.Add(19.5); dia2.Add(16.5); dia2.Add(14); //media2 15.6
